3cb91f5bc6b3d18ef161ba1471e2adcb0d325894
ratface-d <167274773+ratface-d@users.noreply.github.com>
2024-10-19 16:01:07 -0700

    Add files via upload

---
+++ ed/emacs/init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file
+ (expand-file-name
+  "scratchpad.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(catppuccin-theme no-littering gcmh general projectile magit vterm python-mode lua-mode lsp-ui lsp-mode company ivy-prescient prescient which-key evil rainbow-delimiters org-superstar all-the-icons doom-modeline dracula-theme gruvbox-theme modus-themes counsel ivy auto-package-update)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
