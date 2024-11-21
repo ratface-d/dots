2f1490fa017860eee8812c966e254285b3b77b60
ratface-d <dacs1256@gmail.com>
2024-11-21 00:04:44 -0800

    updated emacs

--- ed/emacs/init.el
+++ ed/emacs/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(move-text gruber-darker-theme lsp-ui rainbow-delimiters org-superstar all-the-icons doom-modeline gruvbox-theme ef-themes modus-themes org-roam company prescient counsel ivy auto-package-update)))
+   '(emacsql-sqlite move-text gruber-darker-theme lsp-ui rainbow-delimiters org-superstar all-the-icons doom-modeline gruvbox-theme ef-themes modus-themes org-roam company prescient counsel ivy auto-package-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
