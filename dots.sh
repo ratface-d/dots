f57be7f20d816852d390474d33252ed17d350da3
ratface-d <dacs1256@gmail.com>
2024-11-27 19:46:38 -0800

    updated emacs

--- dots.sh
+++ dots.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-echo "running git add ."
-
-git add . 
-
-echo "running git commit. please type in commit mesage."
-
-read commitMessage
-
-git commit -m '$commitMessage'
-
-echo "running git push. please enter username and git token"
-
-echo "press enter here to see user/token input"
-
-git push
+/home/rat/personal/dots.sh
